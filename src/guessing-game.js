39739560c2b9aed033c71c087f44e6e74d0fdffc
nemaego <nemaego@yandex.ru>
2024-10-22 21:50:06 +0700

    add guessing game class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minValue = null;
+        this.maxValue = null;
+        this.currentNumber = null;
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.currentNumber = Math.round((this.minValue + this.maxValue) / 2);
+        return this.currentNumber;
     }
 
     lower() {
-
+        this.maxValue = this.currentNumber;
     }
 
     greater() {
-
-    }
+        this.minValue = this.currentNumber;
 }
-
+}
 module.exports = GuessingGame;
